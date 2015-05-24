1f1379a081fc503f61f938c0d1408eb49b512fcc
eevahaap <eeva.haapakangas@helsinki.fi>
2015-05-25 00:10:04 +0300

    tietokantojen alustus

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,32 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE opettaja
+(
+id serial primary key,
+nimi varchar(50),
+tunnus varchar(20),
+salasana varchar(20)
+);
+
+CREATE TABLE oppilas
+(
+id serial primary key,
+etunimi varchar(50),
+sukunimi varchar(50),
+luokka varchar(5)
+);
+
+CREATE TABLE oppiaine
+(
+id serial primary key,
+nimi varchar(255),
+oppilas_id integer references oppilas(id) on delete cascade on update cascade,
+opettaja_id integer references opettaja(id) on delete cascade on update cascade
+);
+
+CREATE TABLE arvosana
+(
+id serial primary key,
+arvosana varchar(2),
+oppilas_id integer references oppilas(id) on delete cascade on update cascade,
+opettaja_id integer references opettaja(id) on delete cascade on update cascade
+);
+
